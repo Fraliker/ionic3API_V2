cf8b354506e8447854c6d47a5a8345891d5df666
Sparsh <sendtosparsh@gmail.com>
2017-10-03 16:36:38 +0545

    Add files via upload

---
+++ src/pages/item/item.ts
@@ -0,0 +1,113 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http, HttpModule } from '@angular/http';
+//import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
+
+/**
+ * Generated class for the ItemPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-item',
+  templateUrl: 'item.html',
+})
+export class ItemPage {
+  section;
+  sectionId;
+  itemId;
+  item_info:any;
+  item_img :any;
+  item_desc:any;
+  item_name:any;
+  items:Array<any> = [];
+  item_bottom_list:Array<any> = [];
+  video_link:string;
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams, 
+              public http:Http) {
+    this.sectionId = navParams.get("sectionId");
+    this.itemId = navParams.get("itemId");
+    console.log("item id " + this.itemId);
+    this.http.get('http://ec2-54-169-228-245.ap-southeast-1.compute.amazonaws.com:3000/item/details/'+this.itemId)
+      .subscribe(
+        data=>
+        {
+            this.item_info = data.json();
+            console.log("item log" + data.json());
+            this.item_img = this.item_info[0].item_img;
+            this.item_desc = this.item_info[0].item_desc;
+            this.item_name = this.item_info[0].item_name;
+            this.sectionId = this.item_info[0].section_id;
+            this.section = this.item_info[0].item_section;
+            this.video_link = this.item_info[0].item_video;
+            console.log("item section id" + this.sectionId);
+        },
+        error=>
+        {
+          console.log("err");            
+        });
+   
+    this.http.get('http://ec2-54-169-228-245.ap-southeast-1.compute.amazonaws.com:3000/item/list/' + this.sectionId)
+      .subscribe(
+        data =>
+        {
+            this.items = data.json();
+            console.log(this.items);
+            for(let index = 0; index < this.items.length; index++)
+            {
+              // console.log("item id index " + index + " " + this.items[index].item_id);
+              if(this.items[index].item_id == this.itemId)
+              {
+                console.log("identical");
+                continue;
+              }else
+              {
+                this.item_bottom_list.push(this.items[index]);
+                console.log("push " + this.items[index].item_id);
+              }
+            }
+        },
+        error=>
+        {
+          console.log("err");
+        });
+
+        console.log("bottom list " + this.item_bottom_list);
+        
+}
+
+  gotoSearch()
+  {
+    this.navCtrl.push("SearchPage");
+  }
+
+  ionViewDidLoad()
+  {
+    console.log('ionViewDidLoad ItemPage');
+  }
+
+/*
+  playVideo() {
+    console.log("video link " + this.video_link);
+    var link = this.video_link.match("v=([a-zA-Z0-9_\-]+)&?")[1];
+    console.log("link " + link);
+    // var videoId = link.match("videoId=([a-zA-Z0-9\-_]+)&?");
+    // console.log("videoId " + videoId);
+    // var vId = videoId ? videoId[1] : "";
+    // this.youtube.openVideo(vId);
+      this.youtube.openVideo(link);
+  }
+*/
+
+  gotoItem(itemId)
+  {
+    this.navCtrl.push("ItemPage", {
+        section: this.section,
+        itemId: itemId,
+        sectionId: this.sectionId
+    });
+  }
+}
---
+++ src/pages/search/search.ts
@@ -0,0 +1,144 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { ItemPage } from '../item/item';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
+/**
+ * Generated class for the SearchPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-search',
+  templateUrl: 'search.html',
+})
+export class SearchPage {
+  searchQuery: string = '';
+  items:Array<any> =[];
+  item_name:Array<any>=[];
+  item_info;
+  item_id;
+  section_id;
+  item_length:number = 0;
+  item_name_temp:Array<any> = [];
+  options: BarcodeScannerOptions;
+  results: {};
+
+  constructor( private barcode:BarcodeScanner,
+               public navCtrl: NavController, 
+               public navParams: NavParams, 
+               public http:Http,
+               public viewCtrl:ViewController) {
+  this.http.get('http://ec2-54-169-228-245.ap-southeast-1.compute.amazonaws.com:3000/item/')
+        .subscribe(
+        data =>
+        {
+          this.items = data.json();
+          console.log(this.items);
+          console.log("length " + this.items.length);
+
+        },
+        error =>
+        {
+          console.log("error");
+        });
+    console.log("length value1 : " + this.item_length);
+    this.initializeItems();
+    console.log("length value2 : " + this.item_length);
+            
+}
+
+  initializeItems(){
+   this.item_name.length=0;    
+   this.item_name_temp.length=0;
+    for(let index = 0; index < this.items.length; index++)
+    {
+      this.item_name.push(this.items[index].item_name);
+      this.item_name_temp.push(this.items[index].item_name);
+    }
+   this.item_length = this.items.length;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SearchPage');
+  }
+  
+   getItems(ev: any) {
+    
+    // Reset items back to all of the items
+    this.initializeItems();
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+    console.log("length value4 : " + this.item_length);
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.item_name = this.item_name.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+   
+}
+
+async scanBarcode(){
+   this.options={
+   prompt: 'Scan a barcode to see the result!'
+}
+    const results = await this.barcode.scan(this.options);
+    console.log(this.results);
+    
+    var qrScanInput = results.text.split('/');
+    var itemId = qrScanInput[0];
+    var sectionId = qrScanInput[1];
+
+    this.navCtrl.push("ItemPage", {itemId: itemId, sectionId: sectionId}).then(() => {
+        // first we find the index of the current view controller:
+        const index = this.viewCtrl.index;
+        // then we remove it from the navigation stack
+        this.navCtrl.remove(index);
+      });
+}
+
+
+  getItemId(name, http:Http)
+  {
+ this.http.get('http://ec2-54-169-228-245.ap-southeast-1.compute.amazonaws.com:3000/item/search/'+name)
+        .subscribe(
+          data=>
+          {
+              this.item_info = data.json();
+              console.log("search data " + this.item_info);
+              this.item_id = this.item_info[0].item_id;
+              this.section_id = this.item_info[0].section_id;
+              console.log("item " + this.item_info + " " + this.section_id);
+          }
+        )
+      console.log("search name "+ name);
+      console.log("search id "+ this.item_id);
+  }
+
+
+
+  gotoItem(item_name)
+  {
+    var index:number;
+      for( index = 0; index < this.item_name_temp.length;index++)
+      {
+        if(this.item_name_temp[index] == item_name)
+          break;
+      }
+      
+      this.navCtrl.push("ItemPage", {
+        itemId : this.items[index].item_id,
+        sectionId : this.items[index].section_id
+      }).then(() => {
+        // first we find the index of the current view controller:
+        const index = this.viewCtrl.index;                                                                                                                                                                                                                                                                    
+        // then we remove it from the navigation stack
+        this.navCtrl.remove(index);
+      });
+  }
+
+}
